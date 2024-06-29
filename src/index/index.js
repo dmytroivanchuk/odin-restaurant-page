252e1f13b17b09b11334ca0b508ab56da1556c05
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-06-29 12:42:30 +0300

    feat: configure index page

--- src/index/index.js
+++ src/index/index.js
@@ -1,2 +1,26 @@
 import "./preflight.css";
 import "./index.css";
+
+function configureIndex() {
+  const body = document.querySelector("body");
+  const header = document.createElement("header");
+  const nav = document.createElement("nav");
+  const homeButton = document.createElement("button");
+  homeButton.textContent = "Home";
+  const menuButton = document.createElement("button");
+  menuButton.textContent = "Menu";
+  const contactButton = document.createElement("button");
+  contactButton.textContent = "Contact";
+
+  const content = document.createElement("div");
+  content.id = "content";
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+  header.appendChild(nav);
+  body.appendChild(header);
+  body.appendChild(content);
+}
+
+configureIndex();
